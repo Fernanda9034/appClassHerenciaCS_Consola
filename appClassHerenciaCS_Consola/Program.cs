540f1c7db62bc6e4cb81036eb92e7a07c9a344af
Usuario <Usuario@DESKTOP-MS0QTK9>
2021-10-12 11:05:25 -0500

    Add project files.

---
+++ appClassHerenciaCS_Consola/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appClassHerenciaCS_Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Punto3D a;
+            a = new Punto3D(100, 55.5, 0);
+            Console.WriteLine(a.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ appClassHerenciaCS_Consola/Punto3D.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appClassHerenciaCS_Consola
+{
+    class Punto3D : Punto2D
+    {
+        protected double c;
+
+        public double C
+        {
+            get { return c; }
+            set { c = value; }
+        }
+        private double distance;
+
+        public double Distance
+        {
+            get { return distance; }
+            set { distance = value; }
+        }
+        public Punto3D() : base()
+        {
+            c = 0;
+        }
+        public Punto3D(double x, double y, double c) : base (x, y)
+        {
+            this.c = c;
+            distance = Math.Sqrt(Math.Pow(x - y, 2));
+        }
+        public override string ToString()
+        {
+            return base.ToString() + " C= " + distance + "  Distancia de X a Y = " + distance;
+        }
+    }
+}
